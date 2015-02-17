7e90d70fc1a11760f2060388c81cd9edc10c6402
ptheys <patrick@opus-software.com.br>
2015-02-16 22:01:27 -0200

    Cache matrix inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to cache matrix inversion
+##
+## Usage example:
+## a <- makeCacheMatrix(<matrix>)   ## Makes a matrix inversion cacheable
+## a$get()                          ## Gets original matrix
+## a$set(<matrix>)                  ## Sets a new matrix
+## b <- cacheSolve(a)               ## Solves matrix inversion using cache if it exists
 
-## Write a short comment describing this function
-
+## Create special matrix that caches matrix inversion (solve function)
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    #set a new matrix and clear cache
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    #get current matrix 
+    get <- function() x
+    #set inversion cache
+    setsolve <- function(solve) s <<- solve
+    #get inersion cache
+    getsolve <- function() s
+    list(set = set, 
+         get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## Get a cached matrix inversion if exists, otherwise cache it's
+## inversions for next calls
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #look for cached inversion
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        #cached inversion exists, return it
+        message("getting cached data")
+        return(s)
+    }
+    #get actual matrix and get it's inversion (solve)
+    data <- x$get()
+    s <- solve(data, ...)
+    #cache it's inversion
+    x$setsolve(s)
+    #returns matrix inversion
+    s
 }
